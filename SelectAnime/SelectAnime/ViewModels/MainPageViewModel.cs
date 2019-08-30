0f819d15f382043973df7cbbce54fea7e0cff4ad
Gabriel Eduardo Acosta Nuñez <gacosta.com.do@outlook.com>
2019-08-30 05:59:28 -0400

    ...

--- SelectAnime/SelectAnime/ViewModels/MainPageViewModel.cs
+++ SelectAnime/SelectAnime/ViewModels/MainPageViewModel.cs
@@ -3,13 +3,30 @@ using SelectAnime.Models;
 
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SelectAnime.ViewModels
 {
   public class MainPageViewModel : BaseViewModel
     {
         public ObservableCollection<Character> Characters { get; set; }
+        public ObservableCollection<Amiibo> Amiibos { get; set; }
+        public ICommand SearchCommand { get; set; }
 
+        public MainPageViewModel()
+        {
+            SearchCommand = new
+                Command(async (Text) =>
+                {
+                    string url = "https://www.amiiboapi.com/api/amiibo/?name={Text}";
+                    var service =
+                    new HttpHelper<Amiibos>();
+                    var amiibos =
+                    await service.GetRestServiceDataAsync(url);
+                    Amiibos = new ObservableCollection<Amiibo>(amiibos.amiibo);
+                });
+        }
         public async Task LoadCHaraters()
         {
             var url = "https://www.amiiboapi.com/api/character";
